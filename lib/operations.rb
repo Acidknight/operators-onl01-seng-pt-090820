fad4a87bbfd938e3e1aa5ab7fcbf8feb37121481
Anthony Leon <acidknights89@gmail.com>
2020-09-10 05:13:56 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
 def unsafe?(speed)
-
+if speed > 60
+  return true
+  elsif speed < 40
+  return true
+  elsif speed < 60 || speed > 40
+  return false
+  
 end
 
 
 
-def not_safe?(speed)
-	
+def not_safe?(speeds)
+  speeds < 40 || speeds > 60 ? true : false
 end
-	
-
-
+end
